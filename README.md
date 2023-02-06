e876d494820292d87742a708bbf8559fe00d3f96
Nadezhda Komarova <grukhinan@gmail.com>
2023-02-07 00:28:34 +0300

    Changed test: expected = actual

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 [![Build status](https://ci.appveyor.com/api/projects/status/e8s9li0188v86d18/branch/main?svg=true)](https://ci.appveyor.com/project/KomarovaN/rest/branch/main)
 
 ## Задача №3: Postman Echo
+
--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -22,6 +22,6 @@ public class PostmanEchoTest {
         // Проверки
         .then()
                 .statusCode(200)
-                .body("data", equalTo("peace")); //world
+                .body("data", equalTo("world"));
     }
 }
